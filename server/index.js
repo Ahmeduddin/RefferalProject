b5dfe296da687ad6a3ff0a9adce032e9f4fe22bd
Ahemd Uddin Usama <ahmeduddinusama@gmail.com>
2019-01-10 12:16:36 +0500

    Added styles

--- server/index.js
+++ server/index.js
@@ -19,14 +19,15 @@ app.get('/', (req, res) => {
 
 app.post('/signIn', (req, res) => {
     console.log(req.body);
-    const refferal = new Refferal()
-    refferal.refferal_code = req.body.refferal_code;
-    refferal.users_id.push(req.body.users_id);
-    refferal.save()
-    .then(() => {
-        console.log('Saved refferal Code')
+    Users.findOne({'name': req.body.name, 'password': req.body.password}, function(err, data) {
+        res.setHeader('Content-Type', 'application/json');
+        
+        if(err) {
+            res.status(404);
+            res.json({message :'failed'})
+        }
+        res.json({message: 'success'})
     })
-    res.json({'message': 'user added'})
 })
 
 app.post('/signUp', async (req, res) => {
@@ -37,22 +38,29 @@ app.post('/signUp', async (req, res) => {
         email: req.body.email,
         refferal_id: req.body.refferal_id
     })
+    res.setHeader('Content-Type', 'application/json');
   
     try {
-        
         //console.log('saved user is ', users)
-        const refferal = new Refferal()
+        const refferal = new Refferal();
         refferal.refferal_code = req.body.refferal_id;
-        if(req.body.isRefferal) {
-            refferal.users_id.push(users._id);
-         }
         const refferalResponse = await refferal.save();
+        console.log('refferal id is ', refferalResponse._id);
         user.refferal_id = refferalResponse._id;
         const users = await user.save();
+        const refferalUser = new Refferal();
+        if(req.body.isRefferal) {
+            console.log('isRefferal Value is ', req.body.isRefferal);
+            //refferalUser.users_id.push(users._id);
+            Refferal.findOneAndUpdate({'refferal_code':req.body.isRefferal}, { $push: {'users_id': users._id} }, { new: true }, function(err, doc) {
+                console.log('doc is ', doc);
+            })
+        }
         // refferal.save()
-        console.log('user added')
+        console.log('user added ', users)
         res.json({'message': 'user added'})
     } catch {
+        res.status(404);
         res.json({'message': 'Not added'})
     }
     
